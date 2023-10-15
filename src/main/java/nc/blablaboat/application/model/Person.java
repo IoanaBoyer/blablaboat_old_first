1f97b2c6c979b0e803747d2a1afa53ff679f4c28
Clementine Galin <galin.clem@gmail.com>
2023-10-15 18:43:15 +1100

    Person : Classe : Définition des attributs, constructeur, getters, setters

--- src/main/java/nc/blablaboat/application/model/Person.java
+++ src/main/java/nc/blablaboat/application/model/Person.java
@@ -1,33 +1,134 @@
 package nc.blablaboat.application.model;
 
-// Modèle de données
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Définition de la classe Person
+ */
 public class Person {
+
+    /**
+     * Générateur de l'identifiant
+     */
+    public static AtomicInteger genId = new AtomicInteger();
+
+    /**
+     * L'identifiant de la personne
+     */
     private int id;
-    private String name;
+
+    /**
+     * Nom de la personne
+     */
+    private String lastname;
+
+    /**
+     * Prénom de la personne
+     */
+    private String firstname;
+
+    /**
+     * L'âge de la personne
+     */
     private int age;
-    
-    public void setName(String name) {
-        this.name = name;
+
+    /**
+     * Mot de passe de la personne
+     */
+    private String password;
+
+    public Person(String lastname, String firstname, int age, String password) {
+        this.id = genId.getAndIncrement();
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.age = age;
+        this.password = password;
     }
-    public String getName() {
-        return name;
-    }
-    public int getAge() {
-        return age;
-    }
+
+    /**
+     * Récupère l'identifiant de la personne
+     * @return id
+     */
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
+
+    /**
+     * Récupère le nom de la personne
+     * @return lastname
+     */
+    public String getLastname() {
+        return lastname;
     }
+
+    /**
+     * Modifie le nom de la personne
+     * @param lastname le nouveau nom de la personne
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * Récupère le prénom de la personne
+     * @return firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * Modifie le prénom de la personne
+     * @param firstname le nouveau prénom de la personne
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * Récupère l'âge de la personne
+     * @return  age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Modifie l'âge de la personne
+     * @param age le nouvel age de la personne
+     */
     public void setAge(int age) {
         this.age = age;
     }
 
-    public String toString() {
-        return "{id: "+ this.id +", name: "+ this.name +", age: "+ this.age +"}";
+    /**
+     * Récupère le mot de passe de la personne
+     * @return password
+     */
+    public String getPassword() {
+        return password;
     }
 
-    // Constructeur, getters, setters, etc.
+    /**
+     * Modifie le mot de passe de la personne
+     * @param password le nouveau mot de passe
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Affiche les informations de la personne
+     * @return la description de la personne
+     */
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", age=" + age +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
